39dae6366ff3d7c108e7abbdc9a41c6bf17f5cf5
Josh <selmlocke@gmail.com>
2023-10-01 14:27:53 -0400

    composition vs DI inheritance hybrid

---
+++ example/Program.cs
@@ -0,0 +1,77 @@
+﻿class DataModule
+{
+    public int x;
+    public int y;
+
+    public int b;
+    public int c;
+
+    public DataModule()
+    {
+        x = 4;
+        y = 5;
+
+        b = 7;
+        c = 8;
+    }
+
+};
+
+class addition:DataModule
+{
+    public int add(int x_, int y_)
+    {
+        return x_ + y_;
+    }
+}
+class Subtraction:DataModule
+{
+    public int subtract(int b, int input_)
+    {
+         return b - input_;
+    }
+}
+class Multiplication:DataModule
+{
+    public string multiply(int c, int input_)
+    {
+        c *= input_;
+        return c.ToString();
+    }
+}
+class printing : DataModule
+{
+    public void print(string input)
+    {
+        Console.WriteLine(input);
+    }
+}
+
+class Processing
+{
+    addition adder = new addition();
+    Subtraction subber = new Subtraction();
+    Multiplication multer = new Multiplication();
+    printing printer = new printing();
+
+    DataModule dm = new DataModule();
+    public void Process()
+    {
+        int temp = adder.add(dm.x, dm.y);
+        int intttemp = subber.subtract(dm.b, temp);
+        string multemp = multer.multiply(dm.c, intttemp);
+        printer.print(multemp);
+    }
+}
+
+class main
+{
+    static void Main(string[] args)
+    {
+        Processing p = new Processing();
+        p.Process();
+        alternate.Processing ap = new alternate.Processing();
+        ap.Process();
+
+    }
+}
---
+++ example/composition.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alternate
+{
+    class addition
+    {
+        public int x = 4;
+        public int y = 5;
+
+        public int result;
+        public addition add(int x_, int y_)
+        {
+            result = x_ + y_;
+            return this;
+        }
+    }
+    class Subtraction
+    {
+        public int b = 7;
+        public int result; 
+        public Subtraction subtract(int b, int input_)
+        {
+            result = b - input_;
+            return this;
+        }
+    }
+    class Multiplication
+    {
+        public int c = 8;
+        public string result = "";
+        public Multiplication multiply(int c, int input_)
+        {
+            c *= input_;
+            result = c.ToString();
+            return this;
+        }
+    }
+    class printing
+    {
+        
+        public void print(string input)
+        {
+            Console.WriteLine(input);
+        }
+    }
+
+    class Processing
+    {
+        addition adder = new addition();
+        Subtraction subber = new Subtraction();
+        Multiplication multer = new Multiplication();
+        printing printer = new printing();
+
+
+        
+        public void Process()
+        {
+            printer.print(multer.multiply(multer.c,subber.subtract(subber.b,adder.add(adder.x, adder.y).result).result).result);
+               
+        }
+    }
+
+}
